3fa364b4bb118c6ff350f65189f44ca3daadadbd
shadowmoonezios <agushifelde@hotmail.com>
2025-09-13 18:00:39 +0300

    feat: initial commit for server/index.js

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// MongoDB connection
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
