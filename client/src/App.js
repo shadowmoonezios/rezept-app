81e9c57e5bf4f1d1d9061ca7cd818d92b531a69f
shadowmoonezios <agushifelde@hotmail.com>
2025-09-13 18:00:34 +0300

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Willkommen zur Rezept-App</h1>
+      <p>Entdecken Sie neue Rezepte!</p>
+    </div>
+  );
+}
+
+export default App;
